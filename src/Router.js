f663b36edfac772c1c16302f755884de0de5b8b2
Tesia Wu <tesiawu@utexas.edu>
2019-05-03 22:43:10 -0500

    added photography and marvel routing

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,8 @@ import {Route, Switch} from 'react-router-dom'
 import Home from './Home.js'
 import Profile from './Profile.js'
 import Interests from './Interests.js'
+import Marvel from './Marvel.js'
+import Photography from './Photography.js'
 import Contact from './Contact.js'
 
 /**
@@ -17,6 +19,8 @@ export default class DefaultRouter extends Component {
       	<Route exact path="/home" component={Home}/>
         <Route exact path="/profile" component={Profile}/>
         <Route exact path="/interests" component={Interests}/>
+        <Route exact path="/marvel" component={Marvel}/>
+        <Route exact path="/photography" component={Photography}/>
         <Route exact path="/contact" component={Contact}/>
       </Switch>
     )
